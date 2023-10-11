7f745fd51c243a9416800347a16f315ecdfeb0e0
Clusters_stars <Clusters_stars@qq.com>
2023-10-11 17:25:15 +0800

    更新排序

--- src/main/java/xyz/starsoc/ranking/data/UpdateRankerMapper.java
+++ src/main/java/xyz/starsoc/ranking/data/UpdateRankerMapper.java
@@ -14,10 +14,7 @@ import xyz.starsoc.object.Ranker;
 import xyz.starsoc.object.UpdateRanker;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class UpdateRankerMapper {
 
@@ -36,6 +33,7 @@ public class UpdateRankerMapper {
     private static final Data data = Data.INSTANCE;
     private static Map<String, Ranker> rankerMap = data.getPersons();
 
+    //TODO 将功能进行拆分然后命名好
 
     public void init() {
         //初始化
@@ -57,24 +55,6 @@ public class UpdateRankerMapper {
         }
     }
 
-    public boolean getRankingUpNow(Group group){
-
-        if(rankingUp.isEmpty()){
-            return false;
-        }
-
-        ForwardMessageBuilder builder = new ForwardMessageBuilder(group);
-        Date dateTime = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        String today = simpleDateFormat.format(dateTime);
-
-        builder.add(config.getBot(),"CloudOJ日榜",new PlainText(message.getPrefixRankingUp().replace("%date%",today)));
-        buildRankingUp(builder);
-        rankingUpMessageNow = builder.build();
-
-        return true;
-    }
-
     public void sendRankingUpNow(){
 
         for(Group group : groupList){
@@ -84,6 +64,21 @@ public class UpdateRankerMapper {
             }
 
         }
+    }
+
+    public boolean getRankingUpNow(Group group){
+
+        if(rankingUp.isEmpty()){
+            return false;
+        }
+
+        ForwardMessageBuilder builder = new ForwardMessageBuilder(group);
+
+        buildRankingUp(0,builder);
+
+        rankingUpMessageNow = builder.build();
+
+        return true;
     }
 
     public void sendRankingUp(){
@@ -97,11 +92,9 @@ public class UpdateRankerMapper {
 
             ForwardMessageBuilder builder = new ForwardMessageBuilder(group);
 
-            Date dateTime = new Date();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-            String yesterday = simpleDateFormat.format(dateTime.getTime() - 86400);
-            builder.add(config.getBot(),"CloudOJ日榜",new PlainText(message.getPrefixRankingUp().replace("%date%",yesterday)));
-            buildRankingUp(builder);
+            long oneDaySecond = 86400*1000;
+
+            buildRankingUp(oneDaySecond,builder);
 
             ForwardMessage build = builder.build();
             group.sendMessage(build);
@@ -114,28 +107,41 @@ public class UpdateRankerMapper {
 
     }
 
-    public void buildRankingUp(ForwardMessageBuilder builder){
+    public void buildRankingUp(long timeChange,ForwardMessageBuilder builder){
+
         Date dateTime = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
         String today = simpleDateFormat.format(dateTime);
+        String time = simpleDateFormat.format(dateTime.getTime() - timeChange);
+        builder.add(config.getBot(),"CloudOJ日榜",new PlainText(message.getPrefixRankingUp().replace("%date%",time)));
 
         rankingUpBuilder(builder);
 
+        //这个可以做实时更新的提示
         builder.add(config.getBot(),"CloudOJ日榜",new PlainText(message.getSuffixRankingUp().replace("%date%",today)));
     }
 
     private void rankingUpBuilder(ForwardMessageBuilder builder){
-
-        int rankUp = 0;
 
         if(rankingUp.isEmpty()){
             builder.add(config.getBot(),"CloudOJ日榜",new PlainText("暂未有人上榜..."));
             return;
         }
 
-        for(UpdateRanker updateRanker : rankingUp.values()){
-            //TODO 更新排序
-            ++rankUp;
+        //进行排序
+        Collection<UpdateRanker> rankers = rankingUp.values();
+        UpdateRanker[] values = rankers.toArray(new UpdateRanker[rankers.size()]);
+        Arrays.sort(values, (o1, o2) -> {
+            //排序规则 score*0.8 + passed*5 + rankUp*0.5 - rank
+            int rankerO1 = rankerMap.get(o1.getUserId()).getRank();
+            int rankerO2 = rankerMap.get(o2.getUserId()).getRank();
+            return -((int) ((o1.getScore() - o2.getScore())*0.8 + (o1.getPassed() - o2.getRank())*5 + (o1.getRank() - o2.getRank())*0.5 - (rankerO1 - rankerO2)));
+        });
+
+        for(int i = 0;i < values.length;i++){
+
+            UpdateRanker updateRanker = values[i];
             String userId = updateRanker.getUserId();
             Ranker ranker = rankerMap.get(userId);
             String name = ranker.getName() + "(" + userId + ")";
@@ -144,6 +150,7 @@ public class UpdateRankerMapper {
             int newRank = ranker.getRank();
             double score = updateRanker.getScore();
             int passed = updateRanker.getPassed();
+            int NO = i+1;
             //TODO 这个可能会出现BUG，后面试试将其进行分开
             builder.add(config.getBot(),"CloudOJ日榜",new PlainText(message.getRankingUp()
                     .replace("%name%",name)
@@ -152,9 +159,15 @@ public class UpdateRankerMapper {
                     .replace("%newRank%",newRank + "")
                     .replace("%scoreUp%",score + "")
                     .replace("%passedUp%",passed + "")
-                    .replace("%rank%",rankUp + "")));
+                    .replace("%rank%",NO + "")));
 
         }
+
+        builder.add(config.getBot(),"CloudOJ日榜",new PlainText("排序规则如下：" +
+                "\nscore*0.8 + passed*5 + rankUp*0.5 - rank" +
+                "\n也就是你获取的分数*0.8 + 你通过的题目*5 + 你排行上升的高度*0.5 - 你所在的排名" +
+                "\n若有好的建议，可以反馈给管理员"));
+
     }
 
     private void updateForward(){
@@ -197,7 +210,7 @@ public class UpdateRankerMapper {
                 boolean isMonitored = ranker.getRank() <= config.getMonitorLimit();
 
                 int rank = updateRanker.getRank();
-                if(rank > config.getRankLimit() || isMonitored){
+                if(rank >= config.getRankLimit() || (isMonitored && rank > 0)){
                     int newRank = ranker.getRank();
                     msg += message.getRankUp()
                             .replace("%name%",name)
@@ -207,14 +220,14 @@ public class UpdateRankerMapper {
                 }
 
                 double score = updateRanker.getScore();
-                if(score > config.getScoreLimit() || isMonitored){
+                if(score >= config.getScoreLimit() || (isMonitored && score > 0)){
                     msg += message.getScoreUp()
                             .replace("%name%",name)
                             .replace("%scoreUp%",score + "");
                 }
 
                 int passed = updateRanker.getPassed();
-                if(passed > config.getPassedLimit() || isMonitored){
+                if(passed >= config.getPassedLimit() || (isMonitored && passed > 0)){
                     msg += message.getPassedUp()
                             .replace("%name%",name)
                             .replace("%passedUp%",passed + "")
